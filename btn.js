d24acabf4f5e50eb94074eeb2f761ea6715987dc
Stanislav Bogdanov <stanislav.tiryoshin@gmail.com>
2023-11-12 19:58:29 +0600

    added comments; separate css file

--- btn.js
+++ btn.js
@@ -1,5 +1,7 @@
 const menuBtn = document.querySelector("#menu-btn");
 
+// На клик по кнопке
 menuBtn.addEventListener("click", () => {
+  // у nav переключаем класс
   document.querySelector("nav").classList.toggle("open");
 });
